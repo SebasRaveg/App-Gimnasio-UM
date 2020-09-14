dbc22544b63d89870b1a179f364649e63770e185
Sebastian Rave <seba339931@gmail.com>
2020-09-14 17:00:54 -0500

    Se crea en Controladores/auth donde se tendra la autorixacion por Token usando JWT (Funciones SingUp y SingIn) y se crea Servicios/index.js donde se creara el Token usando moment para las fechas de expiriacion y en config.js se encuentra el token secreto

---
+++ Api-Rest/Controladores/auth.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const servicios = require('../servicios')
+
+// Funcion para comprobar si existe la autenticacion
+function isAuth(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(403).send({message: 'No tienes autorizacion'})
+    }
+
+    const token = req.headers.authorization.split(" ")[1]
+
+    servicios.decodeToken(token)
+    .then(response => {
+        req.usuario = response
+        next()
+    })
+    .catch(response => {
+        res.status(response.status)
+    })
+}
+
+module.exports = isAuth
