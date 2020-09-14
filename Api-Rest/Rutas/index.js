26707fb2e004e4aa4e0d4dc3c44a98660cd3630e
Sebastian Rave <seba339931@gmail.com>
2020-09-14 16:32:17 -0500

    Se crea app.js donde se encuentra la configuracion de Express

---
+++ Api-Rest/Rutas/index.js
@@ -0,0 +1,35 @@
+// Aca se encuentran las rutas de peticiones
+
+'use strict'
+
+const express = require('express')  
+const personaCtrl = require('../controladores/persona')
+const usuarioCtrl = require('../controladores/usuario')
+const auth = require('../middlewares/auth')
+const api = express.Router()
+
+/* ---------------- PETICIONES ------------------ */
+
+//Devolver todos los Datos
+api.get('/persona', auth, personaCtrl.getPersonas)
+ 
+//Buscar Datos por su ID unico
+api.get('/persona/:personaId', personaCtrl.getPersona)
+
+//Almacenar Datos
+api.post('/persona', auth, personaCtrl.savePersona) 
+api.post('/signup', usuarioCtrl.signUp)
+api.post('/signin', usuarioCtrl.signIn)
+
+//Actualizar Datos
+api.put('/persona/:personaId', auth,  personaCtrl.updatePersona)
+
+//Eliminar Datos
+api.delete('/persona/:personaId', auth, personaCtrl.deletePersona)  
+
+//Autenticacion Middleware
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({message: 'Tienes Acceso'})
+})
+
+module.exports = api
---
+++ Api-Rest/app.js
@@ -0,0 +1,37 @@
+// Aca se encuentra la Funcionalidad de EXPRESS
+
+'use strict'
+
+//lee en la carpeta node_modules y lo importa
+const express = require('express')  
+
+// Se lee y se importa Express usa MiddleWare (capas que se van abriendo y al haber un peticion HTTP se va a esas distintas capas)
+const bodyParser = require('body-parser') 
+
+//Configuracion de la App
+const hbs = require('express-handlebars')
+const app = express() 
+const cors = require('cors')
+const api = require('./rutas')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false })) //Propiedad MIDDLEWARE
+app.use(bodyParser.json()) //Peticiones cuerpo de mensaje en formato .json
+
+// Fontaneria, configurar el motor de plantilla en express
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+
+app.use('/api', api) //Se carguen las apis
+app.get('/login', (req, res) => { //Se cargue la vista
+    res.render('login')
+})
+
+app.get('/', (req, res) => {
+    res.render('persona')
+})
+
+module.exports = app
