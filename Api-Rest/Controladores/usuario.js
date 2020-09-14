af96a25164d2c69b2a0b97936ae36036567d3309
Sebastian Rave <seba339931@gmail.com>
2020-09-14 17:08:36 -0500

    Se crea el MiddleWare/auth.js de autenticacion, de los usuarios del controlador y en Controladores/usuario, donde se encuentran las funciones SingIn y SingUp

---
+++ Api-Rest/Controladores/usuario.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Usuario = require('../modelo/usuario')
+const servicio = require('../servicios')
+
+//Registro y autenticacion de usuarios
+function signUp(req, res) {
+    const usuario = new Usuario({
+        email: req.body.email,
+        displayName: req.body.displayName,
+        password: req.body.password
+    })
+
+    usuario.save((err) => {
+        if (err) res.status(500).send({message: `Error al crear el usuario: ${err}`})
+
+        return res.status(200).send({token: servicio.createToken(usuario)})
+    })
+}
+
+function signIn(req, res) {
+    Usuario.find({email: req.body.email}, (err, usuario) => {
+        if (err) return res.status(500).send({message: err})
+        if (!usuario) return res.status(404).send({message: 'No existe el usuario'})
+
+        req.usuario = usuario
+        res.status(200).send({
+            message: 'Te has logueado correctamente', 
+            token: servicio.createToken(usuario)
+        })
+    })
+}
+
+module.exports = {
+    signUp,
+    signIn
+}
